48c5931cee12a361a8fdd498125f869dfd4362b7
hildelio <hildelio@gmail.com>
2024-01-15 23:45:12 -0300

    Req 05

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
@@ -1,21 +1,28 @@
 package com.betrybe.trybnb.ui.views.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.betrybe.trybnb.R
+import com.betrybe.trybnb.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(
-            com.betrybe.trybnb.R.layout.fragment_profile,
-            container,
-            false
-        )
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentProfileBinding.bind(view)
+
+        binding.loginButtonProfile.setOnClickListener {
+            val login = binding.loginInputProfile.editText?.text.toString()
+            val password = binding.passwordInputProfile.editText?.text.toString()
+
+            validation(binding, login, password)
+        }
+    }
+    private fun validation(binding: FragmentProfileBinding, login: String, password: String) {
+        binding.apply {
+            loginError.isVisible = login.isEmpty()
+            passwordError.isVisible = password.isEmpty()
+        }
     }
 }

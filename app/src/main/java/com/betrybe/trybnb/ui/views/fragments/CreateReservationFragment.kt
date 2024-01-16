46244bcb952f0d29f8d11c5e4555a81668fdcfc3
hildelio <hildelio@gmail.com>
2024-01-16 13:23:38 -0300

    Req 09

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.betrybe.trybnb.databinding.FragmentCreateReservationBinding
 
 class CreateReservationFragment : Fragment() {
+    private var binding: FragmentCreateReservationBinding? = null
+    private val createReservationBinding get() = binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
-            com.betrybe.trybnb.R.layout.fragment_create_reservation,
-            container,
-            false
-        )
+        binding = FragmentCreateReservationBinding.inflate(inflater, container, false)
+        return createReservationBinding.root
     }
 }
